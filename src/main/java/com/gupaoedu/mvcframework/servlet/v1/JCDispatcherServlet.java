4edb416b8d990bab4e89fc34f2463f60c175cd2c
pengjunchang <pengjunchang@ingbaobei.com>
2019-04-17 18:34:07 +0800

    doInstance

--- src/main/java/com/gupaoedu/mvcframework/servlet/v1/JCDispatcherServlet.java
+++ src/main/java/com/gupaoedu/mvcframework/servlet/v1/JCDispatcherServlet.java
@@ -1,5 +1,9 @@
 package com.gupaoedu.mvcframework.servlet.v1;
 
+import com.gupaoedu.mvcframework.annotation.GPAutowired;
+import com.gupaoedu.mvcframework.annotation.GPController;
+import com.gupaoedu.mvcframework.annotation.GPService;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @Classname JCDispatcherServlet
@@ -24,6 +27,8 @@ public class JCDispatcherServlet extends HttpServlet {
     private Properties properties = new Properties();
 
     private List<String> classNames = new ArrayList<String>();
+
+    private Map<String,Object> ioc = new HashMap<String, Object>();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -52,17 +57,84 @@ public class JCDispatcherServlet extends HttpServlet {
 
     }
 
+
     private void doAutowired() {
+        //自动依赖注入
+        if (ioc.isEmpty()){return;}
+
+        for (Map.Entry<String, Object> entry : ioc.entrySet()) {
+            //Declared 所有的，特定的 字段，包括private/protected/default
+            //正常来说，普通的OOP编程只能拿到public的属性
+            Field[] fields = entry.getValue().getClass().getDeclaredFields();
+            for (Field field : fields) {
+                if (!field.isAnnotationPresent(GPAutowired.class)){ continue; }
+                GPAutowired autowired = field.getAnnotation(GPAutowired.class);
+
+                //如果用户没有自定义beanName，默认就根据类型注入
+                //这个地方省去了对类名首字母小写的情况的判断，这个作为课后作业
+                //小伙伴们自己去完善
+
+                String beanName = autowired.value().trim();
+                if ("".equals(beanName)){
+                    //获得接口的类型，作为key待会拿这个key到ioc容器中去取值
+                    beanName = field.getType().getName();
+                }
+
+
+            }
+        }
 
     }
 
     private void doInstance() {
         //初始化,为DI准备
-        if(classNames.isEmpty()){
-            return;
+        if(classNames.isEmpty()){ return; }
+
+        for (String className : classNames) {
+            try {
+                Class<?> clazz = Class.forName(className);
+                //注解类才需要初始化
+                if (clazz.isAnnotationPresent(GPController.class)){
+                    Object instance = clazz.newInstance();
+                    //spring默认首字母小写
+                    String beanName = toLowerFirstCase(clazz.getSimpleName());
+                    ioc.put(beanName,instance);
+                }else if (clazz.isAnnotationPresent(GPService.class)){
+                    GPService service = clazz.getAnnotation(GPService.class);
+                    String beanName = service.value();
+                    if ("".equals(beanName)){
+                        beanName = toLowerFirstCase(clazz.getSimpleName());
+                    }
+
+                    Object instance = clazz.newInstance();
+                    ioc.put(beanName,instance);
+                    //根据类型自动赋值,投机取巧的方式
+                    for (Class<?> i : clazz.getInterfaces()) {
+                        if (ioc.containsKey(i.getName())){
+                            throw new Exception("The " + i.getName() + " is exists!");
+                        }
+                        //把接口的类型直接当成key了
+                        ioc.put(i.getName(),instance);
+                    }
+
+                }else {
+                    continue;
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
 
 
+    }
+
+    private String toLowerFirstCase(String simpleName) {
+        char[] chars = simpleName.toCharArray();
+        chars[0] += 32;
+        return String.valueOf(chars);
     }
 
     private void doScanner(String scanPackage) {
